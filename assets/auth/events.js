90ecb6dd9c41ddd4127e1221a0a25cf82aa093a9
j0nathan <jonathan.j.dejesus85@gmail.com>
2018-07-22 01:23:06 -0400

    Updated login proocess
    The user can now choose whether to sign in or sign up. The appropriate modal is displayed and all is
    removed on successful login.

--- assets/auth/events.js
+++ assets/auth/events.js
@@ -8,45 +8,52 @@ const authTemplate = require('../scripts/templates/auth.handlebars')
 
 $('.content').append(authTemplate)
 
+
 const onSignUp = function (event) {
   event.preventDefault()
   console.log('Sign up ran!')
-
+  
   const data = getFormFields(this) // this === event.target 
   api.signUp(data)
-    .then(ui.signUpSuccess)
-    .catch(ui.failure)
+  .then(ui.signUpSuccess)
+  .catch(ui.failure)
 }
 
 const onSignIn = function (event) {
   event.preventDefault()
   console.log('Sign in ran!')
-
+  
   const data = getFormFields(this) // this === event.target 
   api.signIn(data)
-    .then(ui.signInSuccess)
-    .catch(ui.failure)
+  .then(ui.signInSuccess)
+  .catch(ui.failure)
   
 }
 
 const onChangePassword = function (event) {
   event.preventDefault()
   console.log('Change Password ran!')
-
+  
   const data = getFormFields(this) // this === event.target 
   console.log(data)
   api.changePassword(data)
-    .then(ui.changePasswordSuccess)
-    .catch(ui.failure)
+  .then(ui.changePasswordSuccess)
+  .catch(ui.failure)
 }
 
 const onSignOut = function (event) {
   event.preventDefault()
   console.log('Sign put ran!')
-
+  
   api.signOut()
-    .then(ui.signOutSuccess)
-    .catch(ui.failure)
+  .then(ui.signOutSuccess)
+  .catch(ui.failure)
+}
+
+const displayModal = (event) => {
+  event.target.id === "up" ? $(".sign-up-modal").css("display", "block") && $(".sign-in-modal").css("display", "none") 
+  : $(".sign-in-modal").css("display", "block") && $(".sign-up-modal").css("display", "none")
+
 }
 
 const addHandlers = () => {
@@ -54,6 +61,8 @@ const addHandlers = () => {
   $('#sign-in-form').on('submit', onSignIn)
   $('#change-password').on('submit', onChangePassword)
   $('#sign-out-form').on('submit', onSignOut)
+  $('#up').click(displayModal)
+  $('#in').click(displayModal)
 }
 
 module.exports = {
