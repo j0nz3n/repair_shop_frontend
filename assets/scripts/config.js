2a5de2cd00b523c923f5d9227b9b88573a612673
j0nathan <jonathan.j.dejesus85@gmail.com>
2018-07-26 00:47:12 -0400

    Removed trailing / from heroku URL

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://ancient-fjord-10943.herokuapp.com/',
+  production: 'https://ancient-fjord-10943.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
